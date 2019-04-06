a914c8ecfbee720aa921c1c44f383d98cbc43a41
aboud20-meet <aboud20@meet.mit.edu>
2019-04-06 18:04:27 +0300

    bugodlucrnivnkl

--- app.py
+++ app.py
@@ -3,7 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "You are home!"
+
+    return render_template("home.html")
+
+
+
+@app.route('/shop')
+def shop_page():
+	fruitlist = ['apple' , 'banana', 'mango','pineapple', 'pear', 'orange', 'melon']
+	return render_template("shop.html", fruits = fruitlist)
+
+
 
 if __name__ == '__main__':
    app.run(debug = True)
